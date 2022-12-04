57f47bc1256cfff8809d366f61f4a8cf97569222
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2022-12-04 07:51:34 +0000

    :|

--- MessagingService/Bootstrapper/DomainEventHandlerRegistry.cs
+++ MessagingService/Bootstrapper/DomainEventHandlerRegistry.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using BusinessLogic.EventHandling;
 using Lamar;
 using Microsoft.Extensions.Configuration;
