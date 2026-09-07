4354d2cb5934cd824c1fff2956006a71422eabcd
StuartFerguson <stuart_ferguson1development@outlook.com>
2026-09-07 12:12:54 +0100

    support local configs

--- MessagingService/Program.cs
+++ MessagingService/Program.cs
@@ -76,6 +76,7 @@ namespace MessagingService
                     .AddJsonFile($"/home/txnproc/config/appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
                     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"/home/txnproc/config/appsettings.local.json", optional: true, reloadOnChange: true)
                     .AddEnvironmentVariables();
 
                 // Build a snapshot of configuration so we can use it immediately (e.g. for Sentry)
